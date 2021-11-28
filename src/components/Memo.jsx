92d720daaac327f5fe0a8c562e0a119723a0d7aa
ykudnn <ykudnyksb@gmail.com>
2021-11-28 23:22:19 +0900

    memoのメモを追記

--- src/components/Memo.jsx
+++ src/components/Memo.jsx
@@ -2,7 +2,12 @@ export const Memo = () => {
 
   const text = `
   ここにメモをかきたいけど、画面に表示した時に改行できない。
-
+  レンダリング最適化のmemoではない。
+  ・Core Web Vitals：Googleが提唱
+  ・メモ化：処理結果を保持して処理を高速化する技術
+  ・コンポーネントのメモ化：memo
+  ・関数のメモ化：useCallback
+  ・変数のメモ化：useMemo
   `;
 
   return (
