f2e0348262abb57773ae0685d3a629b0b5ab3524
ykudnn <ykudnyksb@gmail.com>
2021-11-28 16:40:13 +0900

    component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+//const App = () => {
+export const App = () => {
+//  return null;
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>aa</p>
+    </div>
+  )
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import ReactDOM from "react-dom";
+import { App } from "./App";
 
-const App = () => {
-//  return null;
-return <h1>Hello world!</h1>;
-};
+// const App = () => {
+// //  return null;
+//   return (
+//     <div>
+//       <h1>Hello world!</h1>
+//       <p>aa</p>
+//     </div>
+//   )
+// };
 
 ReactDOM.render(<App />, document.getElementById("root"));
