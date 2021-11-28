fafd7822cd8ec59df0c9e6a266dda12280c2be65
ykudnn <ykudnyksb@gmail.com>
2021-11-28 20:11:34 +0900

    useEffect 追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@
 
 //import reactDom from "react-dom";
 import { useState } from "react";
+import { useEffect } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 //const App = () => {
 export const App = () => {
   const [num, setNum] = useState(0);
+
+  //useEffect(実行する関数, [依存する値]) ...「ある値が変わった時に限り、ある処理を実行する」
+  //numが変わった時に限り、alertを実行する。
+  useEffect(() => {
+    alert("numの値が変更されました。");
+  }, [num]);
+
   const onClickButton = () => {
-    // alert();
     setNum(num + 1);
   };
 
   return (
     <>
       {console.log(`レンダリング！${num + 1}回目`)}
-      <h1 style={{ color:"red" }}>Hello world!</h1>
+      <h1 style={{color:"red"}}>Hello world!</h1>
       {/* <ColoredMessage color="blue" message="テスト！　ブルー" /> */}
       {/* <ColoredMessage color="pink" message="テスト！　ピンク" /> */}
       <ColoredMessage color="blue">ブルー（childrenで指定）</ColoredMessage>
