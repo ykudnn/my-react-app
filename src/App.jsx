26d260d17a5bd038ef4e8f16968879b3d812aea8
ykudnn <ykudnyksb@gmail.com>
2021-11-28 17:21:19 +0900

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 //memo コンポーネントファイルの拡張子はjsxにする
 
+import reactDom from "react-dom";
+
 //const App = () => {
 export const App = () => {
   const onClickButton = () => {
     alert();
   };
+  const contentStyle = {
+    color:"blue",
+    fontSize:"10px"
+  };
 //  return null;
   return (
     <>
       {console.log("テスト！なみかっこで囲むとJavaScriptを記述できる。")}
-      <h1>Hello world!</h1>
-      <p>aa</p>
+      <h1 style={{ color:"red" }}>Hello world!</h1>
+      <p style={contentStyle}>test!!!!!!!!!!!!!!!!!</p>
       <button　onClick={onClickButton}>ボタン</button>
     </>
   )
