e14b98b7db2bd07fb2f7864123082b65fb4d0ced
ykudnn <ykudnyksb@gmail.com>
2021-11-28 19:25:58 +0900

    ColoredMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 //memo コンポーネントファイルの拡張子はjsxにする
 
-import reactDom from "react-dom";
+//import reactDom from "react-dom";
+import { ColoredMessage } from "./components/ColoredMessage";
 
 //const App = () => {
 export const App = () => {
   const onClickButton = () => {
     alert();
   };
-  const contentStyle = {
-    color:"blue",
-    fontSize:"10px"
-  };
-//  return null;
+
   return (
     <>
       {console.log("テスト！なみかっこで囲むとJavaScriptを記述できる。")}
       <h1 style={{ color:"red" }}>Hello world!</h1>
-      <p style={contentStyle}>test!!!!!!!!!!!!!!!!!</p>
+      <ColoredMessage color="blue" message="テスト！　ブルー" />
+      <ColoredMessage color="pink" message="テスト！　ピンク" />
       <button　onClick={onClickButton}>ボタン</button>
     </>
   )
