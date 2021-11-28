7c5b6427c244f9cba1175e4e756a15ade76a3bc6
ykudnn <ykudnyksb@gmail.com>
2021-11-28 19:37:50 +0900

    分割代入と省略記法

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,8 +1,13 @@
 export const ColoredMessage = (props) => {
+
+  // Propsを分割代入
+  const { color, children } = props;
+
   const contentStyle = {
-    color: props.color,
+    // color: color, 
+    color, //省略記法が使える！
     fontSize: "20px"
   };
 
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 }
